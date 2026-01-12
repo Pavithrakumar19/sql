f21ee661eae8c05c5a4a648541d204ff78f14dce
Pavithra Kumar <kumarnadarpavithra@gmail.com>
2026-01-12 09:06:47 +0530

    Add files via upload

---
+++ BASICS/basic_commands_in_mysql.sql
@@ -0,0 +1,11 @@
+#show all existing databases
+show databases;
+
+#Creating a databse
+CREATE DATABASE college_db;
+
+#using the created db
+USE college_db;
+
+#droping database
+DROP database college_db;
